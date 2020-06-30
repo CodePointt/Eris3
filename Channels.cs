c86ccf02d1c665b761dcfd115b94cd34c9465e6f
waclyu <66577870+waclyu@users.noreply.github.com>
2020-06-30 15:15:24 -0400

    Add files via upload

---
+++ Channels.cs
@@ -0,0 +1,64 @@
+﻿using Discord.WebSocket;
+using Discord;
+using System;
+using System.IO;
+using System.Timers;
+using System.Collections.Generic;
+
+// This is ready to use now. It is set to save every 5 minutes and when the program closes. It is also set to save the data
+// in a file called "LogChannels.json" within the same folder as the executeable
+
+namespace Eris1.Settings
+{
+    public static class Channels
+    {
+        private static readonly string JsonFilePath = Path.Combine(AppContext.BaseDirectory, "Channels.json"); // You can change this if you want.
+        private static Dictionary<ulong, GuildChannelInfo> ChannelDict;
+        private static Timer _saveTimer = new Timer();
+
+        public static void Initialize()
+        {
+            ChannelDict = Helpers.JsonManager.LoadJsonDict<ulong, GuildChannelInfo>(JsonFilePath);
+
+            _saveTimer.Interval = TimeSpan.FromMinutes(5).TotalMilliseconds;
+            _saveTimer.AutoReset = true;
+            _saveTimer.Elapsed += SaveTimerElapsed;
+            _saveTimer.Start();
+        }
+
+        private static void SaveTimerElapsed(object sender, ElapsedEventArgs e) => Save();
+        public static void Save() => Helpers.JsonManager.SaveJson(JsonFilePath, ChannelDict);
+
+        public static GuildChannelInfo GetChannels(ulong guildId)
+        {
+            if (!ChannelDict.ContainsKey(guildId)) // If the dictionary doesn't contain an entry for the guild
+                return new GuildChannelInfo();
+            return ChannelDict[guildId];
+        }
+
+        public static void SetLogChannel(ulong guildId, ulong channelId)
+        {
+            if (!ChannelDict.ContainsKey(guildId))
+                ChannelDict.Add(guildId, new GuildChannelInfo  {  LogChannelId = channelId });
+            else
+                ChannelDict[guildId].LogChannelId = channelId;
+        }
+        public static void SetWelcomeChannel(ulong guildId, ulong channelId)
+        {
+            if (!ChannelDict.ContainsKey(guildId))
+                ChannelDict.Add(guildId, new GuildChannelInfo { WelcomeChannelId = channelId });
+            else
+                ChannelDict[guildId].WelcomeChannelId = channelId;
+        }
+
+
+    }
+
+    public class GuildChannelInfo
+    {
+        public ulong WelcomeChannelId { get; set; } = 0;
+        public ulong LogChannelId { get; set; } = 0;
+    }
+
+
+}
---
+++ Roles.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Discord.WebSocket;
+using Eris1.Helpers;
+using System.Timers;
+
+namespace Eris1.Settings
+{
+    public static class Roles
+    {
+        private static readonly string FilePath = Path.Combine(AppContext.BaseDirectory, "Roles.json");
+        private static Dictionary<ulong, GuildRoleSettings> roleDict;
+        private static Timer _saveTimer = new Timer();
+
+        public static void Initialize()
+        {
+            roleDict = JsonManager.LoadJsonDict<ulong, GuildRoleSettings>(FilePath);
+
+            _saveTimer.Interval = TimeSpan.FromMinutes(5).TotalMilliseconds;
+            _saveTimer.AutoReset = true;
+            _saveTimer.Elapsed += (sender, args) => Save();
+            _saveTimer.Start();
+        }
+
+        public static GuildRoleSettings GetRoleSettings(ulong guildId)
+        {
+            if (!roleDict.ContainsKey(guildId))
+                return new GuildRoleSettings();
+            return roleDict[guildId];
+        }
+        public static void SetWarnRole(ulong guildId, ulong roleId)
+        {
+            if (!roleDict.ContainsKey(guildId))
+                roleDict.Add(guildId, new GuildRoleSettings { WarnRoleId = roleId });
+            else
+                roleDict[guildId].WarnRoleId = roleId;
+        }
+        public static void SetMuteRole(ulong guildId, ulong roleId)
+        {
+            if (!roleDict.ContainsKey(guildId))
+                roleDict.Add(guildId, new GuildRoleSettings { MuteRoleId = roleId });
+            else
+                roleDict[guildId].MuteRoleId = roleId;
+        }
+
+        public static void Save() => JsonManager.SaveJson(FilePath, roleDict);
+    }
+    public class GuildRoleSettings
+    {
+        public ulong MuteRoleId = 0;
+        public ulong WarnRoleId = 0;
+    }
+}
